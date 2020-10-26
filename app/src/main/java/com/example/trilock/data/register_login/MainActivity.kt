0f4ec4ee2c77a48bfb61725c30de4adc4a38f2f6
LukasSteffensen <44091533+LukasSteffensen@users.noreply.github.com>
2020-10-26 13:06:16 +0100

    hej

--- app/src/main/java/com/example/trilock/data/register_login/MainActivity.kt
+++ app/src/main/java/com/example/trilock/data/register_login/MainActivity.kt
@@ -1,16 +1,23 @@
-package com.example.trilock.data.model
+package com.example.trilock.data.register_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.trilock.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val button_register: Button = findViewById<Button>(R.id.testButton)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
 
+        button_register.setOnClickListener {
+
+        }
 
     }
 }
