8dfa50de01eb3805f981bc3a9f46144847e1ceab
tehsnah <jimmerjc@gmail.com>
2020-10-26 13:28:52 +0100

    main activity

--- app/src/main/java/com/example/trilock/data/register_login/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/trilock/data/register_login/ui/home/HomeFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.trilock.data.model.R
+import com.example.trilock.R
 
 class HomeFragment : Fragment() {
 
@@ -22,7 +22,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val textView: TextView = root.findViewById(R.id.text_lock_status)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
