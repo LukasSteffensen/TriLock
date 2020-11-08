c40c61fa63b335d59011f7d43f248ea7c0ded585
maima <maimaih0708@gmail.com>
2020-11-08 16:27:38 +0100

    Recycler view people

--- app/src/main/java/com/example/trilock/data/register_login/ui/notifications/PeopleViewModel.kt
+++ app/src/main/java/com/example/trilock/data/register_login/ui/notifications/PeopleViewModel.kt
@@ -3,6 +3,8 @@ package com.example.trilock.data.model.ui.people
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class PeopleViewModel : ViewModel() {
 
