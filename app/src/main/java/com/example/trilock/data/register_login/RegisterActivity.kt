c525c4d73a34dc2258fb41593ee6957fc948e32b
LukasSteffensen <lukas.steffensen@hotmail.com>
2020-11-29 12:16:26 +0100

    fixed registreigninf users

--- app/src/main/java/com/example/trilock/data/register_login/RegisterActivity.kt
+++ app/src/main/java/com/example/trilock/data/register_login/RegisterActivity.kt
@@ -141,10 +141,12 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun addUserToDatabase() {
+        uid = auth.currentUser!!.uid
         db.collection("users")
         .document(uid).set(user)
         .addOnSuccessListener {
             Log.d("RegisterActivity: ", "DocumentSnapshot added with ID: $uid")
+            auth.signOut()
         }
         .addOnFailureListener { e ->
             Log.w("RegisterActivity: ", "Error adding document", e)
@@ -162,11 +164,9 @@ class RegisterActivity : AppCompatActivity() {
                         ?.addOnCompleteListener(this) {
                             if (task.isSuccessful) {
                                 toast("A verification email has been sent")
-                                uid = auth.currentUser?.uid.toString()
                                 addUserToDatabase()
                             }
                         }
-                    auth.signOut()
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
                 } else {
